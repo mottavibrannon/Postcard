10be66b258c299e137a686b6a81b7ec107d40c7e
Michael Ottavi-Brannon <mottavibrannon@gmail.com>
2014-10-14 21:16:30 -0700

    Added nameTextLabel

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
     
     //@IBOutlet weak var enterStreetTextField: UITextField!
     //@IBOutlet weak var enterCityTextField: UITextField!
@@ -38,7 +39,14 @@ class ViewController: UIViewController {
         
         enterMessageTextField.text = "";
         enterMessageTextField.resignFirstResponder()
-        sendMessageButton.setTitle("Message Sent!", forState: UIControlState.Normal)
+        
+        nameLabel.hidden = false;
+        nameLabel.text = enterNameTextField.text;
+        nameLabel.textColor = UIColor.blueColor();
+        enterNameTextField.text = "";
+        enterNameTextField.resignFirstResponder();
+        
+        sendMessageButton.setTitle("Message Sent...", forState: UIControlState.Selected);
     }
 
 }
